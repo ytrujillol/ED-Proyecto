a22501ccd9093bfdfaf24684f9192eeffb78db44
Adrian <adespitiab@unal.edu.co>
2025-11-23 18:01:46 -0500

    Implementación de la clase ListaEnlazada

---
+++ ED-Proyecto/src/ed/proyecto/ListaEnlazada.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ed.proyecto;
+
+/**
+ *
+ * @author adria_6
+ */
+public class ListaEnlazada<T> {
+    Nodo<T> head;
+    int size;
+
+    public ListaEnlazada() {
+        head = null;
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+    
+    public void insert(T data){
+        Nodo<T> newNode = new Nodo<>(data);
+        if(head == null){
+            head = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        size++;
+    }
+    
+    public boolean remove(int index){
+        boolean removed = false;
+        if(index >= size){
+            System.out.println("No se puede eliminar. Indice mayor o igual que el tamaño");
+        } else {
+            Nodo node = head;
+            for(int x=0; x <= index; x++){
+                node = node.next;
+            }
+            
+            if (node == head){
+                head = head.next;
+                removed = true;
+            } else {
+                node.prev.next = node.next;
+                removed = true;
+            }
+        }
+        
+        if (removed) size--;
+        return removed;
+    }
+    
+    public boolean isEmpty(){
+        return size == 0;
+    }
+}
