5939fdb12f52b9edb8c5b41baac241f8efdfd634
scontrerast2 <scontrerast@unal.edu.co>
2025-11-23 23:06:13 -0500

    tut

---
+++ Tutores.java
@@ -0,0 +1,61 @@
+import jsource.ListaEnlazada.java;
+class Tutores(){
+  //atributos
+  private String documento;
+  private String nombre;
+  private String correoInstitucional;
+  private ListaEnlazada<String> horariosDisponibles;
+  private ListaEnlazada<String> asignaturasQueDicta;
+  public Tutores(String documento, String nombre, String correoInstitucional){
+    this.documento=documento;
+    this.nombre=nombre;
+    this.correoInstitucional=correoInstitucional;
+    this.horariosDisponibles=new ListaEnlazada();
+    this.asignaturasQueDicta=new ListaEnlazada();
+  }
+  //setters y getters
+  public String getDocumento() {
+    return documento;
+  }
+
+  public void setDocumento(String documento) {
+    this.documento = documento;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public String getCorreoInstitucional() {
+    return correoInstitucional;
+  }
+
+  public void setCorreoInstitucional(String correoInstitucional) {
+    this.correoInstitucional = correoInstitucional;
+  }
+  public ListaEnlazada<String> getHorariosDisponibles() {
+    return horariosDisponibles;
+  }
+
+  public ListaEnlazada<String> getAsignaturasQueDicta() {
+    return asignaturasQueDicta;
+  }
+//agregar y eliminar en las listas
+  public void agregarHorario(String horario){
+    horariosDisponibles.insert(horario);
+  }
+  public void agregarAsignatura(String asignatura){
+    asignaturasQueDicta.insert(asignatura);
+  }
+  public void eliminarHorario(String horario){
+    horariosDisponibles.remove(find(horario));
+  }
+  public void eliminarAsignatura(String asignatura){
+    asignaturasQueDicta.remove(find(asignatura));
+  }
+  
+}
