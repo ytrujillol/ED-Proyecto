5f1cef0dbad73a5262b0fc7eaba6fcc31f8492b2
Gabriel Velez <gvelezg@unal.edu.co>
2025-12-06 21:56:56 -0500

    Creación de datos preliminares
    Se instanciaron estudiantes,tutores y tutorias para llenar los mapas con datos preliminares.
    Con estos datos preliminares se creo el gestor de tutorias que recibe esos mapas

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
+        
         // Instancia de 24 estudiantes         (documento,nombre,correo,semestre)
         Estudiante e1 = new Estudiante  ("1001022001", "Carlos Peréz","cperez@unal.edu.co", 2);
         Estudiante e2 = new Estudiante  ("1001022002", "Alejandra Gonzalez", "agonzalez@unal.edu.co", 7);
@@ -29,25 +30,115 @@ public class Main {
         Estudiante e24 = new Estudiante ("1001022024", "Cesar Rivera", "crivera@unal.edu.co", 6);
                                               
         //Instancia de 6 tutores       (documento,nombre,correo) 
-        Tutor tut1 = new Tutor("1001022025", "Omar Rincon", "orincon@unal.edu.co"); //Calculo Diferencial y Álgebra Lineal
-        Tutor tut2 = new Tutor("1001022026", "Isabela Rodriguez", "irodriguez@unal.edu.co"); //Cálculo Diferencial y Cálculo Integral
-        Tutor tut3 = new Tutor("1001022027", "Liseth Ramirez", "lramirez@unal.edu.co");//Lectoescritura e Idiomas
-        Tutor tut4 = new Tutor("1001022028", "Daniel Zamudio", "dzamudio@unal.edu.co");//Programación Básica y POO
-        Tutor tut5 = new Tutor("1001022029", "Jeison Rojas", "jrojas@unal.edu.co");//Circuitos eléctricos y Electrónica Digital
+        Tutor tut1 = new Tutor("1001022025", "Omar Rincon", "orincon@unal.edu.co"); //Cálculo Diferencial y Álgebra Lineal
+        tut1.agregarAsignatura("Cálculo Diferencial");
+        tut1.agregarAsignatura("Álgebra Lineal");
+        
+        Tutor tut2 = new Tutor("1001022026", "Isabela Rodriguez", "irodriguez@unal.edu.co"); //Cálculo Diferencial y Álgebra Lineal
+        tut2.agregarAsignatura("Cálculo Diferencial");
+        tut2.agregarAsignatura("Álgebra Lineal");
+        
+        Tutor tut3 = new Tutor("1001022027", "Liseth Ramirez", "lramirez@unal.edu.co");//Programación Básica y Programación Avanzada
+        tut3.agregarAsignatura("Programación Básica");
+        tut3.agregarAsignatura("Programación Avanzada");
+        
+        Tutor tut4 = new Tutor("1001022028", "Daniel Zamudio", "dzamudio@unal.edu.co");//Programación Básica y Programación Avanzada
+        tut4.agregarAsignatura("Programación Básica");
+        tut4.agregarAsignatura("Programación Avanzada");
+        
+        Tutor tut5 = new Tutor("1001022029", "Jeison Rojas", "jrojas@unal.edu.co");//Fisica I y Fisica II
+        tut5.agregarAsignatura("Física I");
+        tut5.agregarAsignatura("Física II");
+        
         Tutor tut6 = new Tutor("1001022030", "Paula Gomez", "pgomez@unal.edu.co");//Fisica I y Fisica II
+        tut6.agregarAsignatura("Física I");
+        tut6.agregarAsignatura("Física II");
         
         //Instancia de 10 tutorías (idEstudiante, idTutor, asignatura, horario, prioridad, fecha)
-        Tutoria t1 = new Tutoria("1001022001", "1001022025", "Cálculo Diferencial", "11:00 am - 1:00 pm", 3, LocalDate.of(2025,11,23));
+        Tutoria t1 = new Tutoria("1001022001", "1001022025", "Cálculo Diferencial", "11:00 am - 1:00 pm", 3, LocalDate.of(2024,11,23));
+        t1.setEstado("Finalizada");
         Tutoria t2 = new Tutoria("1001022002", "1001022026", "Cálculo Diferencial", "9:00 am - 11:00 am", 4, LocalDate.of(2025,9,3));
-        Tutoria t3 = new Tutoria("1001022003", "1001022027", "Idiomas", "2:00 pm - 4:00 pm", 1, LocalDate.of(2025,10,11));
-        Tutoria t4 = new Tutoria("1001022004", "1001022028", "Programación Orientada a Objetos", "11:00 am - 1:00 pm", 3, LocalDate.of(2025,12,4));
-        Tutoria t5 = new Tutoria("1001022005", "1001022029", "Electrónica Digital", "7:00 am - 9:00 am", 2, LocalDate.of(2025,11,29));
-        Tutoria t6 = new Tutoria("1001022006", "1001022030", "Física I", "2:00 pm - 4:00 pm", 5, LocalDate.of(2025,10,31));
-        Tutoria t7 = new Tutoria("1001022007", "1001022025", "Cálculo I", "9:00 am - 11:00 am", 2, LocalDate.of(2025,12,10));
-        Tutoria t8 = new Tutoria("1001022008", "1001022025", "Cálculo I", "9:00 am - 11:00 am", 3, LocalDate.of(2025,11,7));
-        Tutoria t9 = new Tutoria("1001022009", "1001022026", "Cálculo I", "7:00 am - 9:00 am", 1, LocalDate.of(2025,11,13));
-        Tutoria t10 = new Tutoria("1001022010", "1001022027", "Cálculo I", "11:00 am - 1:00 pm", 1, LocalDate.of(2025,10,24));
+        t2.setEstado("Activa");
+        Tutoria t3 = new Tutoria("1001022003", "1001022027", "Programación Básica", "2:00 pm - 4:00 pm", 1, LocalDate.of(2024,10,11));
+        t3.setEstado("Finalizada");
+        Tutoria t4 = new Tutoria("1001022004", "1001022028", "Programación Avanzada", "11:00 am - 1:00 pm", 3, LocalDate.of(2025,12,4));
+        t4.setEstado("Activa");
+        Tutoria t5 = new Tutoria("1001022005", "1001022029", "Física I", "7:00 am - 9:00 am", 2, LocalDate.of(2025,11,29));
+        t5.setEstado("Activa");
+        Tutoria t6 = new Tutoria("1001022006", "1001022030", "Física II", "2:00 pm - 4:00 pm", 5, LocalDate.of(2025,10,31));
+        t6.setEstado("Activa");
+        Tutoria t7 = new Tutoria("1001022007", "1001022025", "Álgebra Lineal", "9:00 am - 11:00 am", 2, LocalDate.of(2025,12,10));
+        t7.setEstado("Activa");
+        Tutoria t8 = new Tutoria("1001022008", "1001022025", "Álgebra Lineal", "9:00 am - 11:00 am", 3, LocalDate.of(2025,11,7));
+        t8.setEstado("Activa");
+        Tutoria t9 = new Tutoria("1001022009", "1001022026", "Cálculo Diferencial", "7:00 am - 9:00 am", 1, LocalDate.of(2025,11,13));
+        t9.setEstado("Activa");
+        Tutoria t10 = new Tutoria("1001022010", "1001022027", "Programación Básica", "11:00 am - 1:00 pm", 1, LocalDate.of(2025,10,24));
+        t10.setEstado("Activa");
         
+        //Arrays con los datos preliminares
+        Tutor[] arrayTutores = {tut1, tut2, tut3, tut4, tut5, tut6};
+        Estudiante[] arrayEstudiantes = {e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,e12,e13,e14,e15,e16,e17,e18,e19,e20,e21,e22,e23,e24};
+        Tutoria[] arrayTutorias = {t1,t2,t3,t4,t5,t6,t7,t8,t9,t10};
+        
+        //Creacion de HashMap de estudiantes por documento
+        HashMap<String, Estudiante> estudiantesPorDocumento = new HashMap<>();
+        for (Estudiante e: arrayEstudiantes){
+            String documento = e.getDocumento();
+            estudiantesPorDocumento.put(documento, e);
+        }
+        
+        //Creacion de HashMap de Tutores por asignatura
+        HashMap<String, Tutor[]> tutoresPorAsignatura = new HashMap<>();
+        tutoresPorAsignatura.put("Cálculo Diferencial", new Tutor[]{tut1, tut2});
+        tutoresPorAsignatura.put("Álgebra Lineal", new Tutor[]{tut1, tut2});
+        tutoresPorAsignatura.put("Programación Básica", new Tutor[]{tut3, tut4});
+        tutoresPorAsignatura.put("Programación Avanzada", new Tutor[]{tut3, tut4});
+        tutoresPorAsignatura.put("Física I", new Tutor[]{tut5, tut6});
+        tutoresPorAsignatura.put("Física II", new Tutor[]{tut5, tut6});
+        
+        //Creación de HashMap tutorias por estudiante
+        HashMap<String, ListaEnlazada<Tutoria>> tutoriasEstudiante = new HashMap<>();
+        for (Tutoria t : arrayTutorias){
+            String id = t.getIdEstudiante();
+            if ("Activa".equals(t.getEstado())){
+                if(!tutoriasEstudiante.find(id)){//Si aun no está ese estudiante en el mapa
+                    tutoriasEstudiante.put(id, new ListaEnlazada<Tutoria>());
+                }
+                ListaEnlazada<Tutoria> l = tutoriasEstudiante.get(id);//Una vez puesto, agrega la tutoría activa a la lista
+                l.insert(t);
+            }
+        }
+        
+        //Creacion de HashMap de historico de tutorias por estudiante
+        HashMap<String, ListaEnlazada<Tutoria>> historicoEstudiante = new HashMap<>();
+        for (Tutoria t : arrayTutorias){
+            String id = t.getIdEstudiante();
+            if ("Finalizada".equals(t.getEstado())){
+                if(!historicoEstudiante.find(id)){//Si aun no está ese estudiante en el mapa
+                    historicoEstudiante.put(id, new ListaEnlazada<Tutoria>());
+                }
+                ListaEnlazada<Tutoria> l = historicoEstudiante.get(id);//Una vez puesto, agrega la tutoría finalizada a la lista
+                l.insert(t);
+            }
+        }
+        
+        //Creacion de HashMap de tutorias pendientes por tutor
+        HashMap<String, ArrayMaxHeap> tutoriasPendientes = new HashMap<>();
+        for (Tutoria t: arrayTutorias){
+            if ("Activa".equals(t.getEstado())){
+                //Si aun no está el tutor en el mapa, que lo añada
+                //Luego, obtenemos el hashMap y añadimos la tutoria
+                if(!tutoriasPendientes.find(t.getIdTutor())){
+                    tutoriasPendientes.put(t.getIdTutor(),new ArrayMaxHeap());
+                }
+                ArrayMaxHeap heap = tutoriasPendientes.get(t.getIdTutor());
+                heap.insert(t);
+            }
+        }
+
+        GestorTutorias gestor = new GestorTutorias(estudiantesPorDocumento, tutoriasEstudiante, tutoriasPendientes, historicoEstudiante, tutoresPorAsignatura);
+
+
     }
-  
 }
